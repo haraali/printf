26df0a7b9a7b2997f7e0526c8b45501118847561
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-26 05:34:20 +0000

    ⚡️⚡️ improved perfomance

--- utils.c
+++ utils.c
@@ -8,17 +8,15 @@
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
-    	return (1);
+		return (1);
 	else
-    	return (0);
+		return (0);
 }
-
-
 
 
 /**
 * append_hexa_code - appends ascii to hexadecimal
-* @buffer: input value 
+* @buffer: input value
 * @i: input value
 *@ascii_code: ASSCI
 * Return: 3
@@ -28,7 +26,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	char map_to[] = "0123456789ABCDEF";
 
 	if (ascii_code < 0)
-    	ascii_code *= -1;
+		ascii_code *= -1;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
@@ -47,9 +45,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
-    	return (1);
+		return (1);
 	else
-    	return (0);
+		return (0);
 }
 
 
@@ -62,11 +60,11 @@ int is_digit(char c)
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
-    	return (num);
+		return (num);
 	else if (size == S_SHORT)
-    	return ((short) num);
+		return ((short) num);
 	else
-    	return ((int) num);
+		return ((int) num);
 }
 
 
@@ -79,9 +77,9 @@ long int convert_size_number(long int num, int size)
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
-    	return (num);
+		return (num);
 	else if (size == S_SHORT)
-    	return ((unsigned short) num);
+		return ((unsigned short) num);
 	else
-    	return ((unsigned int) num);
+		return ((unsigned int) num);
 }
